bbb1c1016d9486865e62eb4dd8b8acebc528d9b7
Rustem <mr.rustem071193@gmail.com>
2022-07-06 20:23:54 +0600

    full site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useDispatch } from "react-redux"
+import { Header } from './components/header/Header';
+import { CatalogPage } from './pages/catalog-page/CatalogPage';
+import { NotFoundPage } from './pages/not-found-page/NotFoundPage';
+import { Basket } from "./components/basket/Basket"
+import { GamePage } from "./pages/game-page/GamePage"
+import { LoginPage } from "./pages/login-page/LoginPage"
+import Slider from "./components/slider/Slider"
+import { RegisterPage } from './pages/registration-page/RegistrationPage';
+import { setFilterDataDelete, setInputTextDelete } from "./store/slice/searchFilter"
+
+// HOC
+
+import { RequireAuth } from "./hoc/RequireAuth"
+
 
 function App() {
+  const dispatch = useDispatch()
+  const handleClick = () => {
+    dispatch(setFilterDataDelete())
+    dispatch(setInputTextDelete())
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+      <div onClick={handleClick}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Slider />} />
+          <Route path='/catalog' element={
+            <RequireAuth>
+              <CatalogPage />
+            </RequireAuth>
+          } />
+          <Route path='/basket' element={
+            <RequireAuth>
+              <Basket />
+            </RequireAuth>
+          } />
+          <Route path='/game/:title' element={
+            <RequireAuth>
+              <GamePage />
+            </RequireAuth>
+          } />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 
